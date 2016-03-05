94e956dbbb3e7b67c639470405e7758abfea9cd2
forsaken <alexey2142@mail.ru>
2016-03-06 02:03:43 +1000

    draw ya map and lang settings

--- lib/activeadmin/views/activeadmin_form.rb
+++ lib/activeadmin/views/activeadmin_form.rb
@@ -1,14 +1,35 @@
 module ActiveAdmin
   module Views
     class ActiveAdminForm
-      def latlng
-        insert_tag(LatlngProxy, form_builder)
+      def latlng **args
+        lang = args[:lang] || 'en'
+        case args[:map]
+        when :yandex
+          insert_tag(YandexMapProxy, form_builder, lang)
+        end
       end
     end
 
-    class LatlngProxy < FormtasticProxy
+    class YandexMapProxy < FormtasticProxy
+      def build(form_builder, *args, &block)
+        @lang = args.first
+      end
+
       def to_s
-        ""
+        '<li>' \
+        "<script src=\"https://api-maps.yandex.ru/2.1/?lang=#{@lang}&load=Map\" type=\"text/javascript\"></script>" \
+        '<div id="yandex_map" style="height: 400px"></div>' \
+        '<script type="text/javascript">
+          ymaps.ready(init);
+          var myMap;
+          function init() {     
+              myMap = new ymaps.Map("yandex_map", {
+                  center: [55.76, 37.64],
+                  zoom: 7
+              });
+          }
+        </script>' \
+        '</li>'
       end
     end
   end
