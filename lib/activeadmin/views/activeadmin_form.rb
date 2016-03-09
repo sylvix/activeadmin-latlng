1a43e7af8cc2d466df151035a05d1ccc8738b9b8
forsaken <alexey2142@mail.ru>
2016-03-10 02:52:42 +1000

    add map events: draggable, click

--- lib/activeadmin/views/activeadmin_form.rb
+++ lib/activeadmin/views/activeadmin_form.rb
@@ -18,17 +18,43 @@ module ActiveAdmin
 
       def to_s
         '<li>' \
-        "<script src=\"https://api-maps.yandex.ru/2.1/?lang=#{@lang}&load=Map\" type=\"text/javascript\"></script>" \
+        "<script src=\"https://api-maps.yandex.ru/2.1/?lang=#{@lang}&load=Map,Placemark\" type=\"text/javascript\"></script>" \
         '<div id="yandex_map" style="height: 400px"></div>' \
         '<script type="text/javascript">
-          ymaps.ready(init);
-          var myMap;
-          function init() {     
-              myMap = new ymaps.Map("yandex_map", {
-                  center: [55.76, 37.64],
-                  zoom: 7
-              });
+          getCoordinates = function() {
+            return [
+              parseFloat($("#point_lat").val()) || 43.1200,
+              parseFloat($("#point_lng").val()) || 131.8900,
+            ];
           }
+
+          saveCoordinates = function() {
+            $("#point_lat").val( coords[0].toFixed(10) );
+            $("#point_lng").val( coords[1].toFixed(10) );
+          }
+
+          function init() {     
+            myMap = new ymaps.Map("yandex_map", {
+                center: coords,
+                zoom: 12
+            });
+            myPlacemark = new ymaps.Placemark( coords, {}, { preset: "twirl#redIcon", draggable: true } );
+            myMap.geoObjects.add(myPlacemark);
+            myPlacemark.events.add("dragend", function (e) {      
+              coords = this.geometry.getCoordinates();
+              saveCoordinates();
+            }, myPlacemark);
+            myMap.events.add("click", function (e) {        
+              coords = e.get("coordPosition");
+              saveCoordinates();
+            });
+          }
+
+          var myMap;
+          var coords = getCoordinates();
+
+          ymaps.ready(init);
+          saveCoordinates();
         </script>' \
         '</li>'
       end
