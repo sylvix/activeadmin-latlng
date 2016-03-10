2b53b978e128e8ea632944cfb37f758dea7cec62
forsaken <alexey2142@mail.ru>
2016-03-11 02:39:26 +1000

    refactoring and latlng settings

--- lib/activeadmin/views/activeadmin_form.rb
+++ lib/activeadmin/views/activeadmin_form.rb
@@ -2,25 +2,30 @@ module ActiveAdmin
   module Views
     class ActiveAdminForm
       def latlng **args
+        class_name = form_builder.object.class.model_name.element
         lang = args[:lang] || 'en'
         map  = args[:map]  || :yandex
+        id_lat = args[:id_lat] || "#{class_name}_lat"
+        id_lng = args[:id_lng] || "#{class_name}_lng"
+        height = args[:height] || 400
+
         case map
         when :yandex
-          insert_tag(YandexMapProxy, form_builder, lang)
+          insert_tag(YandexMapProxy, form_builder, lang, id_lat, id_lng, height)
         end
       end
     end
 
     class YandexMapProxy < FormtasticProxy
       def build(form_builder, *args, &block)
-        @lang = args.first
+        @lang, @id_lat, @id_lng, @height = *args
       end
 
       def to_s
-        '<li>' \
+        "<li>" \
         "<script src=\"https://api-maps.yandex.ru/2.1/?lang=#{@lang}&load=Map,Placemark\" type=\"text/javascript\"></script>" \
-        '<div id="yandex_map" style="height: 400px"></div>' \
-        '<script type="text/javascript">
+        "<div id=\"yandex_map\" style=\"height: #{@height}px\"></div>" \
+        "<script type=\"text/javascript\">
           var yandexMapObject = {
             coords: null,
             map: null,
@@ -28,35 +33,35 @@ module ActiveAdmin
 
             getCoordinates: function() {
               return [
-                parseFloat($("#point_lat").val()) || 43.1200,
-                parseFloat($("#point_lng").val()) || 131.8900,
+                parseFloat($(\"##{@id_lat}\").val()) || 55.7522200,
+                parseFloat($(\"##{@id_lng}\").val()) || 37.6155600,
               ];
             },
 
             saveCoordinates: function() {
-              $("#point_lat").val( yandexMapObject.coords[0].toFixed(10) );
-              $("#point_lng").val( yandexMapObject.coords[1].toFixed(10) );
+              $(\"##{@id_lat}\").val( yandexMapObject.coords[0].toFixed(10) );
+              $(\"##{@id_lng}\").val( yandexMapObject.coords[1].toFixed(10) );
             },
 
             init: function() {
               yandexMapObject.coords = yandexMapObject.getCoordinates();
               yandexMapObject.saveCoordinates();
 
-              yandexMapObject.map = new ymaps.Map("yandex_map", {
+              yandexMapObject.map = new ymaps.Map(\"yandex_map\", {
                   center: yandexMapObject.coords,
                   zoom: 12
               });
 
-              yandexMapObject.placemark = new ymaps.Placemark( yandexMapObject.coords, {}, { preset: "twirl#redIcon", draggable: true } );
+              yandexMapObject.placemark = new ymaps.Placemark( yandexMapObject.coords, {}, { preset: \"twirl#redIcon\", draggable: true } );
               yandexMapObject.map.geoObjects.add(yandexMapObject.placemark);
 
-              yandexMapObject.placemark.events.add("dragend", function (e) {      
+              yandexMapObject.placemark.events.add(\"dragend\", function (e) {      
                 yandexMapObject.coords = this.geometry.getCoordinates();
                 yandexMapObject.saveCoordinates();
               }, yandexMapObject.placemark);
 
-              yandexMapObject.map.events.add("click", function (e) {        
-                yandexMapObject.coords = e.get("coords");
+              yandexMapObject.map.events.add(\"click\", function (e) {        
+                yandexMapObject.coords = e.get(\"coords\");
                 yandexMapObject.saveCoordinates();
                 yandexMapObject.placemark.geometry.setCoordinates(yandexMapObject.coords);
               });
@@ -64,8 +69,8 @@ module ActiveAdmin
           }
 
           ymaps.ready(yandexMapObject.init);
-        </script>' \
-        '</li>'
+        </script>" \
+        "</li>"
       end
     end
   end
